dc55dd330f60704bdb46d24a6631dcd8ae058e38
Mark Wiggans <mmw125@gmail.com>
2025-04-21 18:36:42 -0400

    hard code some games

--- src/Schedule.tsx
+++ src/Schedule.tsx
@@ -1,12 +1,20 @@
 import { FormControl, InputGroup, Table } from "react-bootstrap";
-import { default as schedule } from "./assets/schedule.json"
+import { default as schedule } from "./assets/schedule.json";
 import React from "react";
 
-type ScheduleHeader = keyof typeof schedule
-type ScheduleRow = keyof typeof schedule["DATE"]
+type ScheduleHeader = keyof typeof schedule;
+type ScheduleRow = keyof (typeof schedule)["DATE"];
 
-const headerTranslation: Map<ScheduleHeader, string> = new Map([["SCORE.1", "SCORE"]])
-const headers: Array<ScheduleHeader> = ["DATE", "HOME", "SCORE", "AWAY", "SCORE.1"];
+const headerTranslation: Map<ScheduleHeader, string> = new Map([
+    ["SCORE.1", "SCORE"],
+]);
+const headers: Array<ScheduleHeader> = [
+    "DATE",
+    "HOME",
+    "SCORE",
+    "AWAY",
+    "SCORE.1",
+];
 const badTeams = ["TC Gemini", "Chicago Tempest"];
 
 export interface ScheduleProps {
@@ -28,12 +36,20 @@ export interface ManualGameScore {
 
 function buildManualGameScore(row: ScheduleRow): ManualGameScore {
     const happened = schedule["SCORE"][row] !== "-";
-    const homeScore = happened ? parseInt(schedule["SCORE"][row]) : badTeams.includes(schedule["AWAY"][row]) ? 30 : 0;
+    const homeScore = happened
+        ? parseInt(schedule["SCORE"][row])
+        : badTeams.includes(schedule["AWAY"][row])
+            ? 30
+            : 0;
     const homeTryPoint = badTeams.includes(schedule["AWAY"][row]);
-    const awayScore = happened ? parseInt(schedule["SCORE.1"][row]) : badTeams.includes(schedule["HOME"][row]) ? 30 : 0;
+    const awayScore = happened
+        ? parseInt(schedule["SCORE.1"][row])
+        : badTeams.includes(schedule["HOME"][row])
+            ? 30
+            : 0;
     const awayTryPoint = badTeams.includes(schedule["HOME"][row]);
 
-    return {
+    const score: ManualGameScore = {
         date: schedule["DATE"][row],
         homeTeam: schedule["HOME"][row],
         homeScore,
@@ -41,104 +57,175 @@ function buildManualGameScore(row: ScheduleRow): ManualGameScore {
         awayTeam: schedule["AWAY"][row],
         awayScore,
         awayTryPoint,
-        happened
+        happened,
+    };
+
+    switch (row) {
+        case "8":
+            score.homeScore = 76;
+            score.awayScore = 7;
+            score.homeTryPoint = true;
+            break;
+        case "9":
+            score.homeScore = 19;
+            score.awayScore = 39;
+            score.awayTryPoint = true;
+            break;
+        case "10":
+            score.homeScore = 24;
+            score.awayScore = 20;
+            score.homeTryPoint = true;
+            break;
+        case "24":
+            score.homeScore = 5;
+            score.awayScore = 30;
+            score.awayTryPoint = true;
+            break;
+        case "29":
+            score.homeScore = 5;
+            score.awayScore = 30;
+            score.awayTryPoint = true;
+            break;
     }
+    return score;
 }
 
 export function Schedule({ modifyStandings }: ScheduleProps) {
-    const rows: Array<ScheduleRow> = Object.keys(schedule["DATE"]) as Array<ScheduleRow>;
+    const rows: Array<ScheduleRow> = Object.keys(
+        schedule["DATE"]
+    ) as Array<ScheduleRow>;
 
     const [games, setGames] = React.useState<ManualGameScore[]>([]);
 
     React.useEffect(() => {
         const scores: ManualGameScore[] = [];
-        rows.forEach(row => {
+        rows.forEach((row) => {
             scores.push(buildManualGameScore(row));
-        })
+        });
         setGames(scores);
         modifyStandings(scores);
-    }, [setGames])
+    }, [modifyStandings]);
 
     return (
         <Table id="standings" striped bordered responsive>
             <thead>
                 <tr key={"header"}>
-                    {headers.map(key => (<th>{headerTranslation.get(key) ?? key}</th>))}
+                    {headers.map((key) => (
+                        <th>{headerTranslation.get(key) ?? key}</th>
+                    ))}
                 </tr>
             </thead>
             <tbody>
-                {games.map((game, i) => <GameDisplay key={game.date + "key"} game={game} setManualScore={(manualScore) => {
-                    const newScores = [...games];
-                    newScores[i] = manualScore;
-                    setGames(newScores);
-                    modifyStandings(newScores.filter(score => score.homeTeam != ""));
-                }} />)}
+                {games.map((game, i) => (
+                    <GameDisplay
+                        key={game.date + "key"}
+                        game={game}
+                        setManualScore={(manualScore) => {
+                            const newScores = [...games];
+                            newScores[i] = manualScore;
+                            setGames(newScores);
+                            modifyStandings(
+                                newScores.filter((score) => score.homeTeam != "")
+                            );
+                        }}
+                    />
+                ))}
             </tbody>
         </Table>
     );
 }
 
-export function GameDisplay({ game, setManualScore }: { game: ManualGameScore, setManualScore: (manualScore: ManualGameScore) => void }) {
-    const {happened} = game;
+export function GameDisplay({
+    game,
+    setManualScore,
+}: {
+    game: ManualGameScore;
+    setManualScore: (manualScore: ManualGameScore) => void;
+}) {
+    const { happened } = game;
     const buildForm = (header: ScheduleHeader) => {
         const onFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
             const newValue = e.target.value;
             const gameCopy = {
-                ...game
-            }
+                ...game,
+            };
             if (header === "SCORE") {
                 gameCopy.homeScore = parseInt(newValue ? newValue : "0");
             } else if (header === "SCORE.1") {
                 gameCopy.awayScore = parseInt(newValue ? newValue : "0");
             }
-            setManualScore(gameCopy)
+            setManualScore(gameCopy);
         };
 
         const onCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
             const newValue = e.target.checked;
             const gameCopy = {
-                ...game
-            }
+                ...game,
+            };
             if (header === "SCORE") {
                 gameCopy.homeTryPoint = newValue;
             } else if (header === "SCORE.1") {
                 gameCopy.awayTryPoint = newValue;
             }
-            setManualScore(gameCopy)
+            setManualScore(gameCopy);
         };
-        return (<InputGroup>
-            <FormControl type="number" width={"20px"} id="inputGroup-sizing-sm" onChange={onFormChange} defaultValue={header === "SCORE" ? game.homeScore : game.awayScore} />
-            <InputGroup.Checkbox onChange={onCheckboxChange} defaultChecked={header === "SCORE" ? game.homeTryPoint : game.awayTryPoint} />
-        </InputGroup>)
-    }
+        return (
+            <InputGroup>
+                <FormControl
+                    type="number"
+                    width={"20px"}
+                    id="inputGroup-sizing-sm"
+                    onChange={onFormChange}
+                    defaultValue={header === "SCORE" ? game.homeScore : game.awayScore}
+                />
+                <InputGroup.Checkbox
+                    onChange={onCheckboxChange}
+                    defaultChecked={
+                        header === "SCORE" ? game.homeTryPoint : game.awayTryPoint
+                    }
+                />
+            </InputGroup>
+        );
+    };
 
     const hasWinner = game.homeScore != game.awayScore;
     const homeWins = game.homeScore > game.awayScore;
 
     return (
         <tr key={game.date + " " + game.homeTeam}>
-            {headers.map(header => {
+            {headers.map((header) => {
                 if (header === "SCORE") {
-                    return (<td>{happened ? game.homeScore : buildForm(header)}</td>)
+                    return <td>{happened ? game.homeScore : buildForm(header)}</td>;
                 } else if (header === "SCORE.1") {
-                    return (<td>{happened ? game.awayScore : buildForm(header)}</td>)
+                    return <td>{happened ? game.awayScore : buildForm(header)}</td>;
                 }
 
                 if (header === "HOME") {
-                    return (<td style={hasWinner ? { backgroundColor: homeWins ? "green" : "red" } : {}}>
-                        {game.homeTeam}
-                    </td>)
+                    return (
+                        <td
+                            style={
+                                hasWinner ? { backgroundColor: homeWins ? "green" : "red" } : {}
+                            }
+                        >
+                            {game.homeTeam}
+                        </td>
+                    );
                 } else if (header === "AWAY") {
-                    return (<td style={hasWinner ? { backgroundColor: !homeWins ? "green" : "red" } : {}}>
-                        {game.awayTeam}
-                    </td>)
+                    return (
+                        <td
+                            style={
+                                hasWinner
+                                    ? { backgroundColor: !homeWins ? "green" : "red" }
+                                    : {}
+                            }
+                        >
+                            {game.awayTeam}
+                        </td>
+                    );
                 }
 
-                return (<td>
-                    {game.date}
-                </td>)
-            }
-            )}
+                return <td>{game.date}</td>;
+            })}
         </tr>
-    )
+    );
 }
